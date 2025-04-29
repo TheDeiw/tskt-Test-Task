7172f0711b5c63379ac0671987be0b00447a5fb7
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-04-29 20:28:49 +0300

    Made random spawning of objects

---
+++ tskt Test Task/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject player;
+
+    public float minSpawnRadius = 10f;
+    public float maxSpawnRadius = 20f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        float angle = Random.Range(0f, Mathf.PI * 2f);
+
+        float distance = Random.Range(minSpawnRadius, maxSpawnRadius);
+
+        Vector3 offset = new Vector3(Mathf.Cos(angle) * distance, 0f, Mathf.Sin(angle) * distance);
+
+        Vector3 spawnPosition = player.transform.position + offset;
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+}
--- tskt Test Task/Assets/Scripts/Player Movement.cs
+++ tskt Test Task/Assets/Scripts/Player Movement.cs
@@ -11,8 +11,22 @@ public class PlayerMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -26,5 +40,8 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
