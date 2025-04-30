41fd4735df43ebe7b56db1a7a0b7833febcde50b
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-04-30 17:48:53 +0300

    Add Enemies model

--- tskt Test Task/Assets/Scripts/EnemeDestroy.cs
+++ tskt Test Task/Assets/Scripts/EnemeDestroy.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 
 public class EnemeDestroy : MonoBehaviour
 {
+    public GameObject tankGun;
     private GameObject[] enemies;
-
+    public float rotationSpeed = 5f;
+    public PlayerMovement playerMovement;
+    private Vector3 direction;
     void Update()
     {
+        GameObject closest = FindTheClosest();
+        if (closest != null)
+        {
+            direction = closest.transform.position - tankGun.transform.position;
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                tankGun.transform.rotation = Quaternion.Lerp(tankGun.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            }
+        }
+        else
+        {
+            direction = playerMovement.moveDirectionForGun;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            tankGun.transform.rotation = Quaternion.Lerp(tankGun.transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+
         if (Input.GetKeyDown(KeyCode.E))
         {
-            GameObject closest = FindTheClosest();
             if (closest != null)
             {
                 Destroy(closest);
--- tskt Test Task/Assets/Scripts/Player Movement.cs
+++ tskt Test Task/Assets/Scripts/Player Movement.cs
@@ -19,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     bool isGrounded;
 
+    public Animator animator;
+    public Vector3 moveDirectionForGun;
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -31,13 +33,16 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        animator.SetFloat("Speed", direction.magnitude);
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDirectionForGun = moveDirection;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
 
